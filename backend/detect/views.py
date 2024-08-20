22c593a5cd70c65a03ed50f79c568fb1b3a0474e
git-dru <mastercrypto.us@gmail.com>
2024-08-20 14:38:39 +0300

    BE: add detect view

--- backend/detect/views.py
+++ backend/detect/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http.response import JsonResponse
 
-# Create your views here.
+from detect.utils import upload_file
+from detect.service import detect
+
+class DetectAPIView(APIView):
+  def post(self, request):
+    file = request.FILES.get('file')
+    search_text = request.data.get('search_text')
+        
+    if file:
+      uploaded_url = upload_file(file)
+      
+      video_output_path = detect(uploaded_url, search_text)
+      
+      return JsonResponse({
+        "message": "File uploaded successfully",
+        "file_url": video_output_path
+      })
+    else:
+      return JsonResponse({
+        "message": "No file uploaded"
+      }, status=400)
