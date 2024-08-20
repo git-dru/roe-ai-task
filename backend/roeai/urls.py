8d3bfa93430bb0d2646ca12b9f640f9636ffa00b
git-dru <mastercrypto.us@gmail.com>
2024-08-20 14:38:13 +0300

    BE: add detect api endpoint

--- backend/roeai/urls.py
+++ backend/roeai/urls.py
@@ -2,7 +2,7 @@
 URL configuration for roeai project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from detect.views import DetectAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/detect', DetectAPIView.as_view(), name="detect"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
